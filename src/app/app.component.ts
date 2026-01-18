4a07cd64aca55c2442551a1bc68c83d11fa84200
personal <dzulfiqarzakyaulia@gmail.com>
2026-01-18 20:33:55 +0700

    backoffice_angular: move header to shared, add features/auth/login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'employee-app';
-  description = 'Congratulations! Your app is running. 🎉';
-  links = [
-    { title: 'Explore the Docs', link: 'https://angular.dev' },
-    { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
-    { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
-    { title: 'Angular Language Service', link: 'https://angular.dev/tools/language-service' },
-    { title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
-  ]
+
 }
