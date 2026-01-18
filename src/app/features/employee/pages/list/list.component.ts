d5bf92a2e15118929e5f7102174a703bf63dc69a
personal <dzulfiqarzakyaulia@gmail.com>
2026-01-18 22:42:10 +0700

    backoffice_angular: check if crud of employees works, checking delete

--- src/app/features/employee/pages/list/list.component.ts
+++ src/app/features/employee/pages/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { EmployeeService } from '../../../../core/services/employee.service';
 
 @Component({
   selector: 'app-list',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  employeeService = inject(EmployeeService);
+  employees = this.employeeService.allEmployees;
 
+  handleDelete(username: string) {
+    console.log(username)
+    this.employeeService.deleteEmployee(username)
+  }
+
+  handleEdit(username: string) {
+    const employee = this.employeeService.getEmployeeByUsername(username)
+    console.log(employee)
+  }
+
+  handleDetail(username: string) {
+    const employee = this.employeeService.getEmployeeByUsername(username)
+    console.log(employee)
+  }
 }
